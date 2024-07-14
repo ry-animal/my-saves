7fe37572e6c621d0475536ab529531c48d0ad13e
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-13 18:18:18 -0700

    fix linting again

--- src/pages/layout/RootLayout.tsx
+++ src/pages/layout/RootLayout.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 
 interface LayoutProps {
   children: React.ReactNode;
