85714325edd37b057b9d3a694189f9b8c6515502
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-14 18:48:14 -0700

    sitemap fixes

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,11 +1,11 @@
-import { getAllVideoIds } from '@/lib/videos';
+import { getAllVideos } from '@/lib/videos';
 import { GetServerSideProps } from 'next';
 
 const Sitemap: React.FC = () => null;
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
   const baseUrl = 'https://my-saves-seven.vercel.app';
-  const videoIds = await getAllVideoIds();
+  const videoIds = await getAllVideos(1, 6, true);
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -15,7 +15,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
         <changefreq>daily</changefreq>
         <priority>1.0</priority>
       </url>
-      ${videoIds
+      ${videoIds.videos
         .map(
           (id) => `
         <url>
